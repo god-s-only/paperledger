6c9506795f7e707624c9cf025c1478399eb12b44
god-s-only <joshxchris098@gmail.com>
2026-01-15 21:44:58 +0100

    added interceptor for authorization header, apikey and secret not inputted yet

---
+++ app/src/main/java/com/paperledger/app/di/NetworkModule.kt
@@ -0,0 +1,44 @@
+package com.paperledger.app.di
+
+import com.paperledger.app.core.BASE_URL
+import com.paperledger.app.core.alpacaAuthHeader
+import com.paperledger.app.data.remote.api.AlpacaApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideAlpacaApiService(client: OkHttpClient): AlpacaApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(AlpacaApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val apiKey = "YOUR_API_KEY"
+                val apiSecret = "YOUR_API_SECRET"
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Authorization", alpacaAuthHeader(apiKey, apiSecret))
+                    .build()
+                chain.proceed(newRequest)
+            }
+            .build()
+    }
+}
