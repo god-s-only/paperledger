85b0a24d1f1dbad75edc700e015fff59e2d4b569
god-s-only <joshxchris098@gmail.com>
2026-02-22 12:03:27 +0100

    feat: updated room dao

--- app/src/main/java/com/paperledger/app/data/local/PaperledgerDAO.kt
+++ app/src/main/java/com/paperledger/app/data/local/PaperledgerDAO.kt
@@ -1,43 +1,58 @@
 package com.paperledger.app.data.local
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
-import com.paperledger.app.data.local.WatchlistsEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PaperledgerDAO {
-    @Insert
-    suspend fun insertWatchlist(watchlistsEntity: List<WatchlistsEntity>)
-
-    @Query("DELETE FROM watchlists")
-    suspend fun deleteAllWatchlists()
-
-    @Query("SELECT * FROM watchlists")
-    fun observeAllWatchlists(): Flow<List<WatchlistsEntity>>
-
-    @Transaction
-    suspend fun replaceAllWatchlists(watchlistsEntity: List<WatchlistsEntity>) {
-        deleteAllWatchlists()
-        insertWatchlist(watchlistsEntity)
-    }
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPosition(positionEntity: List<PositionEntity>)
-
-    @Query("DELETE FROM positions")
-    suspend fun deleteAllPositions()
 
     @Query("SELECT * FROM positions")
     fun observeAllPositions(): Flow<List<PositionEntity>>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPositions(positions: List<PositionEntity>)
+
+    @Query("DELETE FROM positions")
+    suspend fun deleteAllPositions()
+
     @Transaction
-    suspend fun replaceAllPositions(positionEntity: List<PositionEntity>){
+    suspend fun replaceAllPositions(positions: List<PositionEntity>) {
         deleteAllPositions()
-        insertPosition(positionEntity)
+        insertPositions(positions)
+    }
+
+    @Query("SELECT * FROM orders")
+    fun observeAllOrders(): Flow<List<OrderEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrders(orders: List<OrderEntity>)
+
+    @Query("DELETE FROM orders")
+    suspend fun deleteAllOrders()
+
+    @Transaction
+    suspend fun replaceAllOrders(orders: List<OrderEntity>) {
+        deleteAllOrders()
+        insertOrders(orders)
+    }
+
+
+    @Query("SELECT * FROM account_info LIMIT 1")
+    fun observeAccountInfo(): Flow<AccountInfoEntity?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAccountInfo(accountInfo: AccountInfoEntity)
+
+    @Query("DELETE FROM account_info")
+    suspend fun deleteAccountInfo()
+
+    @Transaction
+    suspend fun replaceAccountInfo(accountInfo: AccountInfoEntity) {
+        deleteAccountInfo()
+        insertAccountInfo(accountInfo)
     }
 }
