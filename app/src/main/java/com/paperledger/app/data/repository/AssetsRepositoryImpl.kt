c618de4c7ff89c1e3ed02ee64c2c999d703b5822
god-s-only <joshxchris098@gmail.com>
2026-02-22 13:47:00 +0100

    feat: refactored assets repository, use AssetsPagingSource

--- app/src/main/java/com/paperledger/app/data/repository/AssetsRepositoryImpl.kt
+++ app/src/main/java/com/paperledger/app/data/repository/AssetsRepositoryImpl.kt
@@ -1,32 +1,42 @@
 package com.paperledger.app.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.google.gson.Gson
 import com.paperledger.app.core.AppError
 import com.paperledger.app.core.mapError
 import com.paperledger.app.data.mappers.assets.toAssetsDomain
+import com.paperledger.app.data.paging.AssetsPagingSource
 import com.paperledger.app.data.remote.api.AlpacaApiService
 import com.paperledger.app.data.remote.dto.error.ErrorResponseDTO
 import com.paperledger.app.domain.models.assets.AssetsModel
 import com.paperledger.app.domain.repository.AssetsRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AssetsRepositoryImpl @Inject constructor(private val alpacaApiService: AlpacaApiService): AssetsRepository {
-    override suspend fun getAllAssets(): Result<List<AssetsModel>> {
-        return withContext(Dispatchers.IO){
-            try {
-                val res = alpacaApiService.getAllAssets()
-                if(res.isSuccessful){
-                    val body = res.body() ?: return@withContext Result.failure(AppError.EmptyBody)
-                    Result.success(body.map { it.toAssetsDomain() })
-                }else{
-                    val errorBody = Gson().fromJson(res.errorBody()?.string(), ErrorResponseDTO::class.java)
-                    Result.failure(AppError.HttpError(res.code(), errorBody.message))
-                }
-            }catch (e: Exception){
-                Result.failure(mapError(e))
+    override suspend fun getAllAssets(searchQuery: String): Flow<PagingData<AssetsModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = PAGE_SIZE,
+                prefetchDistance = PREFETCH_DISTANCE,
+                enablePlaceholders = false,
+                initialLoadSize = INITIAL_LOAD_SIZE
+            ),
+            pagingSourceFactory = {
+                AssetsPagingSource(
+                    alpacaApiService = alpacaApiService,
+                    searchQuery = searchQuery
+                )
             }
-        }
+        ).flow
+    }
+    companion object {
+        private const val PAGE_SIZE = 30
+        private const val PREFETCH_DISTANCE = 10
+        private const val INITIAL_LOAD_SIZE = 60
     }
 }
--- app/src/main/java/com/paperledger/app/domain/repository/AssetsRepository.kt
+++ app/src/main/java/com/paperledger/app/domain/repository/AssetsRepository.kt
@@ -1,7 +1,9 @@
 package com.paperledger.app.domain.repository
 
+import androidx.paging.PagingData
 import com.paperledger.app.domain.models.assets.AssetsModel
+import kotlinx.coroutines.flow.Flow
 
 interface AssetsRepository {
-    suspend fun getAllAssets(): Result<List<AssetsModel>>
+    suspend fun getAllAssets(searchQuery: String): Flow<PagingData<AssetsModel>>
 }
