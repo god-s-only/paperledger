0c458dfe5ac1e40be437da47467198916d2749e7
god-s-only <joshxchris098@gmail.com>
2026-02-22 12:01:29 +0100

    feat: account info updated mappers

--- app/src/main/java/com/paperledger/app/data/mappers/account/AccountInfoMapper.kt
+++ app/src/main/java/com/paperledger/app/data/mappers/account/AccountInfoMapper.kt
@@ -1,13 +1,36 @@
 package com.paperledger.app.data.mappers.account
 
+import com.paperledger.app.data.local.AccountInfoEntity
 import com.paperledger.app.data.remote.dto.account.response.success.AccountResponseDTO
 import com.paperledger.app.domain.models.trade.AccountInfo
 
-fun AccountResponseDTO.toAccountInfo(): AccountInfo{
+fun AccountResponseDTO.toAccountInfo(): AccountInfo {
     return AccountInfo(
         accountId = id,
         currency = currency,
         lastEquity = lastEquity.toDouble(),
+        createdAt = createdAt,
+        status = status,
+        accountType = accountType
+    )
+}
+
+fun AccountResponseDTO.toEntity(): AccountInfoEntity {
+    return AccountInfoEntity(
+        accountId = id,
+        currency = currency,
+        lastEquity = lastEquity.toDouble(),
+        createdAt = createdAt,
+        status = status,
+        accountType = accountType
+    )
+}
+
+fun AccountInfoEntity.toDomain(): AccountInfo {
+    return AccountInfo(
+        accountId = accountId,
+        currency = currency,
+        lastEquity = lastEquity,
         createdAt = createdAt,
         status = status,
         accountType = accountType
