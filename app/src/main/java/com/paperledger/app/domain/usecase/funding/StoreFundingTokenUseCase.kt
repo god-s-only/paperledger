9b208258f4dc48ea06dd79399e1f063ea2b0f214
god-s-only <joshxchris098@gmail.com>
2026-06-02 06:16:00 +0100

    fix: use javax inject instead of jakarta

--- app/src/main/java/com/paperledger/app/domain/usecase/funding/StoreFundingTokenUseCase.kt
+++ app/src/main/java/com/paperledger/app/domain/usecase/funding/StoreFundingTokenUseCase.kt
@@ -1,7 +1,7 @@
 package com.paperledger.app.domain.usecase.funding
 
 import com.paperledger.app.domain.repository.FundingRepository
-import jakarta.inject.Inject
+import javax.inject.Inject
 
 class StoreFundingTokenUseCase @Inject constructor(private val repository: FundingRepository) {
     suspend operator fun invoke(token: String): Result<Unit> {
