9bc1f6a586796733ca2fefc1105e16a08ee4e23c
god-s-only <joshxchris098@gmail.com>
2026-06-02 06:15:17 +0100

    fix: createach relationship use case injects interface repo achrelationshiprepository

--- app/src/main/java/com/paperledger/app/domain/usecase/ach/CreateACHRelationshipUseCase.kt
+++ app/src/main/java/com/paperledger/app/domain/usecase/ach/CreateACHRelationshipUseCase.kt
@@ -1,10 +1,10 @@
 package com.paperledger.app.domain.usecase.ach
 
 import com.paperledger.app.data.remote.dto.ach.request.ACHRelationshipsRequestDTO
-import com.paperledger.app.data.repository.ACHRelationshipRepositoryImpl
+import com.paperledger.app.domain.repository.ACHRelationshipRepository
 import javax.inject.Inject
 
-class CreateACHRelationshipUseCase @Inject constructor(private val repository: ACHRelationshipRepositoryImpl) {
+class CreateACHRelationshipUseCase @Inject constructor(private val repository: ACHRelationshipRepository) {
     suspend operator fun invoke(accountId: String, request: ACHRelationshipsRequestDTO): Result<Unit> {
         return repository.createACHRelationship(request, accountId)
     }
